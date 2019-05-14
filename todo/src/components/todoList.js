e2ecf27f7eef844a4a5fc135dd4bb05ba9df3e10
Melissa Murphy <melissa@thoughtfulmonkey.design>
2019-05-13 22:37:46 -0700

    add toggleItem and map + connection

---
+++ todo/src/components/todoList.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class ToDoList extends Component {
+  state = {  }
+  render() { 
+    return (  );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    todo: state.todo,
+    finished: state.finished
+  }
+}
+ 
+export default connect(
+  mapStateToProps,
+  { addItem, deleteItem, toggleItem }
+)
--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -1,5 +1,6 @@
 const ADD_ITEM = 'ADD_ITEM';
 const DELETE_ITEM = 'DELETE_ITEM';
+const TOGGLE_ITEM = 'TOGGLE_ITEM'
 
 const todo = (state = [], action) => {
   switch (action.type) {
@@ -7,6 +8,8 @@ const todo = (state = [], action) => {
       return { ...state, finished: false };
     case DELETE_ITEM:
       return { ...state, finished: true };
+    case TOGGLE_ITEM:
+      return { ...state, toggleItem: !state.toggleItem };
     default:
       return state;
   }
