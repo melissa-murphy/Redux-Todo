b42eccd8ed38660076a22fe5394343393ac13b68
melissa-murphy <melissamurphy.ca@gmail.com>
2019-05-14 15:15:53 -0700

    addItem and toggleItem actions

--- todo/src/actions/index.js
+++ todo/src/actions/index.js
@@ -1,1 +1,19 @@
-export const 
+const ADD_ITEM = 'ADD_ITEM';
+export const addItem = newItem => {
+    return {
+        type: ADD_ITEM,
+        payload: newItem
+    }
+}
+
+const TOGGLE_ITEM = 'TOGGLE_ITEM'
+export const toggleItem = id => {
+    return {
+        type: TOGGLE_ITEM,
+        payload: id
+    }
+}
+
+
+// const DELETE_ITEM = 'DELETE_ITEM';
+// export function deleteItem()
